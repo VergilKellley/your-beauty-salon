99296c443a25f54ab1b81947be4df886355d3efc
VergilKellley <verg_1@yahoo.com>
2024-12-05 21:33:17 -0600

    first commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- STYLIST CARD SLIDER -->
+    <!-- https://www.youtube.com/watch?v=k-GqSFGZBHM -->
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/draggable-image-slider.css">
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <title>Document</title>
+</head>
+
+<body>
