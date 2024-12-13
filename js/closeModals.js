f36f82ed425f09e7f7ba6c86df29f4315a8a8615
VergilKellley <verg_1@yahoo.com>
2024-12-12 20:27:48 -0600

    first commit

---
+++ js/closeModals.js
@@ -0,0 +1,25 @@
+function closeSuccessModal() {
+    document.getElementById("success-modal").style.display = "none";
+    event.preventDefault()
+};
+
+function closeSelectDateModal() {
+event.preventDefault()
+    document.getElementById("select-date-modal").style.display = "none";
+    event.preventDefault()
+};
+
+function closeStylistNameEmptyModal() {
+    document.getElementById("stylist-name-empty-modal").style.display = "none";
+    event.preventDefault()
+};
+
+function closeStylistNameEmptyModal() {
+    document.getElementById("closse-stylist-name-err-btn").style.display = "none";
+    event.preventDefault()
+};
+
+function closeCheckDateSelectedModal() {
+    document.getElementById("check-appointment-date-modal").style.display = "none";
+    event.preventDefault()
+};
