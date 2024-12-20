e9eb2e4c7f90e0abf3a9924c6d4c1bd3972e6385
VergilKellley <verg_1@yahoo.com>
2024-12-19 21:04:48 -0600

    first commit

---
+++ backend/display_website_colors.php
@@ -0,0 +1,14 @@
+<?php
+require 'db.php';
+//Display Website colors 
+$sql = "SELECT * FROM website_colors;";
+$result = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+if ($resultCheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+    $primary_color = $row['main_color'];
+    $secondary_color = $row['secondary_color'];
+    $accent_color = $row['accent_color'];
+    }
+}
