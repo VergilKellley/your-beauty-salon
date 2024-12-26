945e4096789577a98c4447a2cd36e5fcbc6723d2
VergilKellley <verg_1@yahoo.com>
2024-12-25 19:14:33 -0600

    first commit

---
+++ backend/display_services_chosen.php
@@ -0,0 +1,18 @@
+<?php
+require 'db.php';
+//DISPLAY ABOUT PAGE
+$sql = "SELECT * FROM services_chosen;";
+$result = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+if ($resultCheck > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $service_title = $row['service_title'];
+        $services_description = $row['services_description'];
+        $service_img = $row['service_img'];
+        $service_img_desc = $row['service_img_desc'];
+        $service_video = $row['service_video'];
+
+
+    }
+}
