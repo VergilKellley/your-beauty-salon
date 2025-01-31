d55ad0a56b9f4e6ce3fb374c30a55c48a2d18de6
VergilKellley <verg_1@yahoo.com>
2025-01-30 20:25:10 -0600

    added color variable

--- about.php
+++ about.php
@@ -4,6 +4,7 @@
 
 <div class="about-header">
     <div style="position:absolute;top:75px; bottom:0; right:0; left:0;background:#000; opacity:.5;height:100%"></div>
+    <!-- height (see above) should be 117% not 100% on inmotion hosting -->
     <div style="position:absolute;max-width:700px;width: 100vw;padding: 0 20px; color:#fff">
         <h2 style="position:relative;z-index:3"><?= $about_header_title ?></h2>
         <p style="margin-top:20px;line-height:1.5"><?= $about_header_text ?></p>
@@ -26,24 +27,25 @@
                     <p><?= $our_story_title ?></p>
                     <h1 style="position:relative;z-index: 4;"><?= $our_story_large_title ?></h1>
                     <div class="two-para-text">
-                        <p style="border-left:8px solid red;padding-left:10px"><?= $our_story_paragraph_1 ?></p>
+                        <p style="border-left:8px solid <?= $secondary_color ?>;padding-left:10px"><?= $our_story_paragraph_1 ?></p>
                         <br />
-                        <p style="border-left:8px solid red;padding-left:10px"><?= $our_story_paragraph_2 ?></p>
+                        <p style="border-left:8px solid <?= $secondary_color ?>;padding-left:10px"><?= $our_story_paragraph_2 ?></p>
                         <br />
                         <p>
                             <span style="font-style: italic; font-weight: 600"><?= $about_owner_name ?></span>
                         </p>
                     </div>
+                    <br>
                     <div class="book-now-view-services-btn-container"
                         style="display: flex; justify-content: space-evenly;">
-                        <a class="btn btn-book-appointment" href="book-appointment">BOOK NOW</a>
-                        <a class="btn btn-view-services" href="all-services">VIEW SERVICES</a>
+                        <a style="background:<?= $secondary_color ?>" class="btn" href="book-appointment">BOOK NOW</a>
+                        <!-- <a class="btn btn-view-services" href="all-services">VIEW SERVICES</a> -->
                     </div>
                 </div>
             </div>
         </div>
         <div class="img-and-text-content">
-            <div class="text-under-large-image">
+            <div style="background:<?= $main_color ?>" class="text-under-large-image">
                 <div>
                     <!-- <div>
                         <p>Luxuriously Simple</p>
@@ -62,13 +64,13 @@
                 <div style="min-width:300px;">
                     <h3><?= $core_values_title ?></h3>
                     <br>
-                    <div style="border-left:8px solid red;padding-left:10px">
+                    <div style="border-left:8px solid <?= $secondary_color ?>;padding-left:10px">
                         <h2><?= $core_values_large_title_1 ?></h2>
                         <p><?= $core_values_large_title_text_1 ?></p>
                     </div>
 
                     <br>
-                    <div style="border-left:8px solid red;padding-left:10px">
+                    <div style="border-left:8px solid <?= $secondary_color ?>;padding-left:10px">
                         <h2><?= $core_values_title_2 ?></h2>
                         <p><?= $core_values_large_title_text_2 ?></p>
                     </div>
